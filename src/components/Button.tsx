0894bd78926c2317283bf11b51279024d3286be2
Victor Hernandez <vichdz97@txstate.edu>
2023-06-06 14:32:08 -0500

    fixed indent spacing

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,9 +6,9 @@ interface Props {
 }
 
 function Button({ children, onClick }: Props) {
-  return (
-    <button className="btn btn-danger m-3 p-5" onClick={onClick}>{children}</button>
-  )
+    return (
+        <button className="btn btn-danger m-3 p-5" onClick={onClick}>{children}</button>
+    )
 }
 
 export default Button
